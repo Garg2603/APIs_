87c86e6a6608c9a6d3c5b181c444791b7e3ada16
Tarang Garg <143086130+Garg2603@users.noreply.github.com>
2024-02-07 16:19:50 +0530

    Add files via upload

---
+++ mainn.py
@@ -0,0 +1,34 @@
+from flask import Flask,jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/armstrong/<int:n>')
+def armstrong(n):
+    sum = 0
+    order = len(str(n))
+    copy_n = n
+    while n > 0:
+        digit = n % 10
+        sum += digit ** order
+        n = n // 10
+    if sum == copy_n:
+        print(f"{copy_n} is an Armstrong number")
+        result={
+            "Number":n,
+            "Armstrong":True,
+            "Server IP":"122.14.14.12.1000"
+        }
+    else:
+        print(f"{copy_n} is not an Armstrong number")
+        result={
+            "Number":n,
+            "Armstrong":False,
+            "Server IP":"122.14.14.12.1000"
+        }
+    return jsonify(result)
+
+if __name__=="__main__":
+    app.run(debug=True)
